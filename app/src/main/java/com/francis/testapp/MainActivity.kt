4d54352bfac9401d8eeb0031a9ced796a8293a44
Francis Akpan <francisakpan@apples-MacBook-Pro-3.local>
2021-02-02 06:41:15 +0100

    Update MainActivity

--- app/src/main/java/com/francis/testapp/MainActivity.kt
+++ app/src/main/java/com/francis/testapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.francis.testapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun add(view: View) {
+
+    }
+
+    fun sub(view: View) {
+
+    }
+
+    fun div(view: View) {
+
     }
 }
